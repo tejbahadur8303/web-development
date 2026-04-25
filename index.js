5148ea2d1593b4a75e68852668a5725d1eae8897
tejbahadur8303 <tej9519@gmail.com>
2026-04-25 11:33:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,107 @@
+/*. let js = "amzing";
+if (js == "amzing") alert("thankyou for visiting this page!");
+20 + 20 + 20 + 20;
+console.log(20 + 20 + 20 + 20);
+console.log("Tejash");
+let firstName = "Tejash";
+let first = "hey bro wattsup";
+console.log(first);
+console.log(firstName);
+
+let tej_bahadur = "RMT";
+let $function = 23;
+let name = "tej";
+console.log(tej_bahadur);
+console.log($function);
+console.log(name);
+
+let PI = 3.32;
+console.log(PI);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+javascriptIsFun = 'yes!';
+console.log(javascriptIsFun);
+
+let age = 32;
+age = 31;
+console.log(age);
+
+const ageje = 394 - 321;
+const hht = 222 - 22;
+console.log(ageje, hht);
+
+const first = 'Tej';
+const last = 'bahadur';
+console.log(first + ' ' + last);
+
+
+//Assignment operators
+let x = 10 + 5;
+x += 10;
+x *= 10;
+x++;
+x--;
+console.log(x);
+
+
+
+//// CHALLENGES
+
+const MarksMass = 78;
+const johnMass = 92;
+const MarksHeight = 1.69;
+const johnHeight = 1.95;
+
+let BMImark = (MarksMass) / (MarksHeight * MarksHeight);
+
+let BMIjohn = (johnMass) / (johnHeight * johnHeight);
+
+const markHigherBMI = BMImark > BMIjohn;
+console.log(BMImark, BMIjohn, markHigherBMI);
+if (BMImark > 25) alert("hey Marks  Sorry to tell that you are over weight!!");
+if (BMIjohn > 25) alert("hey Marks  Sorry to tell that you are over weight!!");
+
+
+
+
+//String And templete
+
+const firstName = 'tejash';
+const job = 'teacher';
+const birthYear = 2004;
+const year = 2026;
+const tejsh = "i  am " + firstName + ', a ' + (year - birthYear) + " old "+ job;
+console.log(tejsh);
+
+
+console.log('string with \n\
+    multiple \n\
+    lines');
+    
+
+const age = 9;
+const isOldEnough = age >= 18;
+if (isOldEnough) {
+    console.log('Tejash can start driving license 🚗 ');
+} else {
+    console.log('koi na babu hota hai better luck next time 👍🏼');
+}
+
+
+
+const inputYear = '1992';
+console.log(Number(inputYear), inputYear);
+console.log(inputYear + 18);
+
+*/
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('tej'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+
