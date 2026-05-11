1c88482f5539dd2faa1b484e839a77ac62c29da7
tejbahadur8303 <tej9519@gmail.com>
2026-05-11 20:18:42 +0530

    Add files via upload
    first commit

---
+++ Mini.py
@@ -0,0 +1,11 @@
+# snack = input("Enter Your preffered snack: ").lower()
+# if snack == "cookies" or snack == "samosa":
+#     print(f"Greate Choice! We'l serve you {snack}")
+# else:
+#     print("sorry , we only serve cookies or samosa")
+
+Tea = input("Enter your desire cup size that you to preffered: ").loweer()
+if Tea == "small" or Tea == "midium" or Tea == "large" :
+    print(f"Your order is process {Tea}")
+else:
+    print("Unknown cup size ! Please Enter the correct cup size ")
